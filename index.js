967494979ea9d8bdc3ff0efa843625dd6a853f34
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2019-11-08 17:38:52 +0100

    added the iframe

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-	let btn = $("#top");
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > 300) {
-			btn.style.display = "block";
-		} else {
-			btn.style.display = "none";
-		}
-	});
+// $(document).ready(function() {
+// 	let btn = $("#top");
+// 	$(window).scroll(function() {
+// 		if ($(window).scrollTop() > 300) {
+// 			btn.style.display = "block";
+// 		} else {
+// 			btn.style.display = "none";
+// 		}
+// 	});
 
-	btn.on("click", function(e) {
-		e.preventDefault();
-		$("html, body").animate({ scrollTop: 0 }, "300");
-	});
-});
+// 	btn.on("click", function(e) {
+// 		e.preventDefault();
+// 		$("html, body").animate({ scrollTop: 0 }, "300");
+// 	});
+// });
